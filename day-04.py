adedc85d35c4377a900b44b7624ab26ccc0088f6
Meggan M Green <github@meggangreen.com>
2018-12-31 02:07:44 +0000

    day 4 p1 wip

--- day-04.py
+++ day-04.py
@@ -1,10 +1,43 @@
 """Part One"""
+import re
+
+timestamp_re = re.compile(r"\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2})\]")
+guard_re = re.compile(r"Guard #(\d{1,5}) ")
+sleeps_re = re.compile(r"(falls asleep)")
+wakes_re = re.compile(r"(wakes up)")
 
 class Guard(object):
     def __init__(self, idn):
         self.idn = idn
         self.history = []
         self.total_sleep = 0
+
+
+    def get_history(self):
+        schedule = get_schedule()
+        guard_idn = None
+        for entry in schedule:
+            if guard_re.search(entry):
+                guard_idn = int(guard_re.search(entry).groups()[0])
+            if guard_idn == self.idn:
+                self.history.append(entry)
+
+
+    def calc_total_sleep(self):
+        total = 0
+        for i in range(len(self.history)):
+            if sleeps_re.search(self.history[i]):
+                start = int(self.history[i][15:17])
+                if i+1 != len(self.history) and wakes_re.search(self.history[i+1]):
+                    end = int(self.history[i+1][15:17])
+                else:
+                    end = 60
+                total += end - start
+            else:
+                start, end = None, None
+        self.total_sleep = total
+        return self.total_sleep
+
 
     def get_most_slept_minute(self):
         if not self.history or total_sleep <= 0:
