1778a331cdea0c8b8a4b29ed3cade34f342e4621
Meggan M Green <github@meggangreen.com>
2018-12-31 05:58:45 +0000

    day 5 complete

--- day-05.py
+++ day-05.py
@@ -1,4 +1,6 @@
 """ Part One """
+from string import ascii_uppercase
+
 
 def get_polymer():
     with open('day-05.txt') as file:
@@ -34,3 +36,31 @@ def catalyze_reaction(polymer):
 def collapse_product(reaction):
     product = ''.join([unit for unit in reaction if unit != 0])
     return product
+
+
+""" Part Two """
+def preen_polymer(polymer, unit):
+    preened_polymer = list(polymer)
+    # unit = unit.upper()
+
+    for i in range(len(preened_polymer)):
+        if preened_polymer[i].upper() == unit:
+            preened_polymer[i] = 0
+
+    return ''.join([p for p in preened_polymer if p != 0])
+
+
+def make_all_preened_and_reacted_products(polymer):
+    products = {}
+
+    for char in list(ascii_uppercase):
+        prod = collapse_product(catalyze_reaction(preen_polymer(polymer, char)))
+        if not products.get(len(prod)):
+            products[len(prod)] = []
+        products[len(prod)].append(prod)
+
+    return products
+
+
+def get_shortest_product(polymer):
+    return sorted(make_all_preened_and_reacted_products(polymer).keys())[0]
