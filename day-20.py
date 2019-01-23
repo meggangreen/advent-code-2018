ebf012e855c343d2693248e3806c857167a866de
Meggan M Green <github@meggangreen.com>
2019-01-23 03:29:16 +0000

    day 20 wip

--- day-20.py
+++ day-20.py
@@ -1,5 +1,8 @@
 """ Let's make some trees """
 
+import re
+from collections import deque
+
 class Section(str):
     """ A section of the route in which there are no deviations -- ie each node,
         a letter, has only one child -- because I think it will save in counting.
@@ -22,6 +25,7 @@ class Section(str):
         if not self.children:
             self.children = []
         self.children.extend(children)
+        self.children.sort(key=lambda child: child.start)
 
 
 class Route:
@@ -32,7 +36,80 @@ class Route:
 
 
     def find_most_doors_no_repeat(self):
+        """ DFS shortest path to farthest end """
+
+        section = self.root
+        path = [section]
+        to_visit = deque(section.children)
+
+        # for section in to_visit:
+
+
         def walk_through_doors(section):
+            pass
 
-            section = self.root
 
+""" Part One is just easier to do in regex """
+skip = re.compile(r'\([\w\|]+\|\)')
+opener = re.compile(r'\(')
+closer = re.compile(r'\)[\w]*$')
+level = '()'
+
+with open('day-20.txt', 'r') as file:
+    route = file.read().strip()[1:-1]
+
+def remove_skip(route):
+    match = re.search(skip, route)
+    if match:
+        route = route[:match.span()[0]] + routeroute[match.span()[1]:]
+
+    return route
+
+
+def find_closer(route):
+    match = re.search(closer, route)
+    if match:
+        return match.span()[0]
+
+
+def find_opener(route):
+    match = re.search(opener, route)
+    if match:
+        return match.span()[0] + 1
+
+
+def explore(route):
+
+    # while True:
+    #     length = len(route)
+    #     route = remove_skip(route)
+    #     if length == len(route):
+    #         break
+
+    doors = 0
+
+    if route[-1] == '|':
+        return doors
+
+    start = find_opener(route)
+    if start:
+        end = find_closer(route)
+        route = route[start:end]
+        doors += explore(route)
+
+    doors
+
+
+    r = 0
+    while r < len(route):
+        if route[r] in 'NESW':
+            doors += 1
+        elif route[r] == '(':
+        elif route[r] == ')':
+            # should not arrive?
+            break
+        # elif route[r] == '|' and route[r+1] == ')':
+        #     doors = 0
+        #     break
+
+    return doors
