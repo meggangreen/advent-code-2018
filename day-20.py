db377c53b58dcd44e45616b444375b4c48fb486c
Meggan M Green <github@meggangreen.com>
2019-01-23 07:43:52 +0000

    day 20 changing tactics again

--- day-20.py
+++ day-20.py
@@ -3,17 +3,18 @@
 import re
 from collections import deque
 
-class Section(str):
+class Section:
     """ A section of the route in which there are no deviations -- ie each node,
         a letter, has only one child -- because I think it will save in counting.
         'children' is a list of Sections.
 
     """
 
-    def __init__(self, doors): #, start, children=None):
-        super().__init__()
+    def __init__(self, title, doors): #, start, children=None):
+        self.title = title
+        self.data = doors
         # self.start = float('inf') * -1
-        self.len = self.__len__()
+        # self.len = len(self.data)
         self.children = None
 
 
@@ -74,16 +75,22 @@ def remove_skip(puzzle_in):
     return puzzle_in
 
 
-def find_closer(puzzle_in):
+def find_next_closer(puzzle_in):
     match = re.search(closer, puzzle_in)
     if match:
-        return match.span()[0]
+        return match.start()
+
+
+def find_last_closer(puzzle_in):
+    matches = re.search(closer, puzzle_in)
+    if matches:
+        return 2
 
 
 def find_opener(puzzle_in):
     match = re.search(opener, puzzle_in)
     if match:
-        return match.span()[0] + 1
+        return match.end()  # + 1
 
 
 def explore(puzzle_in):
@@ -99,6 +106,7 @@ def explore(puzzle_in):
 
     else:
         import pdb; pdb.set_trace()
+        # this is not finding my last ')'
         end = find_closer(puzzle_in)
         to_explore = puzzle_in[start:end]
         sections.extend([Section(r) for r in puzzle_in[:start-1].split('|')])
@@ -118,10 +126,6 @@ def balance_parens(puzz_in):
     start = None
     end = -1
 
-    # if there is a path to skip, skip it
-    if puzz_in[-1] == '|':
-        return
-
     for i, p in enumerate(puzz_in):
         if p == '(':
             import pdb; pdb.set_trace()
@@ -133,8 +137,30 @@ def balance_parens(puzz_in):
             start = end + 1
             end = i
             children = [Section(char) for char in puzz_in[start:end].split('|')]
+            # only gets preceeding parents
             parent_level = [Section(char) for char in sections.pop().split('|')]
             parent_level[-1].adopt(children)
+
+
+def make_edges_list(puzz_in, i=0):
+    sections = []
+    children = []
+    end = -1
+
+    start = end + 1
+    end = find_opener(puzz_in)
+    span = puzz_in[start:end]
+    remain = puzz_in[end+1:]
+
+    to_section = span.split('|')
+    for sect in to_section:
+        i += 1
+        sections.append(Section(str(i), sect))
+
+    sections[-1].adopt(make_edges_list(remain, i))
+
+
+
 
 
 
